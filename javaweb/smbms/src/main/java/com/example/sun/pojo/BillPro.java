8d3ccf0d4e48a1c4409c01d240cebc60e71501a0
孙建祥 <1504734652@qq.com>
2023-12-12 23:16:34 +0800

    javaweb项目搭建

---
+++ javaweb/smbms/src/main/java/com/example/sun/pojo/BillPro.java
@@ -0,0 +1,14 @@
+package com.example.sun.pojo;
+
+public class BillPro extends Bill {
+	private String proname;
+
+	public String getProname() {
+		return proname;
+	}
+
+	public void setProname(String proname) {
+		this.proname = proname;
+	}
+	
+}
